77325d0f41ff4fbf680b6176ef6396b0a1f3fb98
pmaroli <pranavmaroli@umail.ucsb.edu>
2017-04-27 16:22:28 -0700

    Wall Follower Code Added

---
+++ Lab03 - Wall Follower/WallFollowerCode/WallFollowerCode.ino
@@ -0,0 +1,75 @@
+#include <AFMotor.h>
+#include <SoftwareSerial.h>
+#define SwitchPin_A 1   // switch A is connected to pin 1
+#define SwitchPin_B 2   // switch B is connected to pin 2
+
+const int rxPin = 13;
+const int txPin = 13;
+const int analogInPin = A0;  // Analog input pin that the SHORT range sensor is attached to
+const int longPin = A1;  // Analog input pin that the LONG range sensor is attached to
+int sensorValue = 0; //Short range sensor value
+int longValue = 0; //Long range sensor value
+
+int count = 0;
+int diff = 0;
+int maxdiff = 30;
+
+SoftwareSerial mySerial =  SoftwareSerial(rxPin,txPin);
+AF_DCMotor right_motor(3, MOTOR12_64KHZ);// create motor #1 64 KHZ pwm
+AF_DCMotor left_motor(4, MOTOR12_64KHZ); // create motor #2, 64KHz pwm
+
+void setup() {
+pinMode( txPin, OUTPUT);
+mySerial.begin(9600); // initialize serial communications at 9600 bps:
+
+left_motor.setSpeed(255); // Speeds are set differently to compensate for difference in motor power
+right_motor.setSpeed(200); 
+}
+
+int setpoint = 300; //Setpoint distance from the wall for the short range sensor
+
+void loop(){ 
+
+  sensorValue = analogRead(analogInPin);
+  longValue = analogRead(longPin);
+
+  // print the results to the serial monitor:
+  mySerial.print("?x00?y0");     //Sets Cursor to x00,y0 
+  mySerial.print("Short = ");
+  mySerial.print(sensorValue);
+  mySerial.print("    ");
+  mySerial.print("?x00?y1");     //Sets Cursor to x00,y0 
+  mySerial.print("Long = ");
+  mySerial.print(longValue);
+  mySerial.print("    ");
+  
+  diff = sensorValue - setpoint;
+
+  if(longValue > 400) {
+    count++;
+    if(count > 3){
+      right_motor.run(FORWARD);
+      left_motor.run(BACKWARD);
+      delay(600);
+      count = 0;
+      left_motor.run(FORWARD);  
+    }
+  }else if(diff < maxdiff && diff > -maxdiff){
+    left_motor.run(FORWARD);
+    right_motor.run(FORWARD);
+  }else if(diff > maxdiff) {
+    count++;
+    if(count > 5){
+      left_motor.run(RELEASE);  
+    }
+  }else if(diff < -maxdiff) {
+    count++;
+    if(count > 5){
+      right_motor.run(RELEASE);  
+    }
+  }else {
+    count = 0;
+  }
+
+  delay(2);
+}
